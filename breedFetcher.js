2caa52a291c3259aa7b10284e51372bde984cc38
Chitra <chitravenkitraman@gmail.com>
2021-09-23 16:56:13 -0600

    mocha test added

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,27 @@
 const request = require('request');
-const args = process.argv.slice(2);
-const breed = 'q='+args;
-const url = 'https://api.thecatapi.com/v1/breeds/search?' + breed;
 
+
+// refactoring request using a function
+const fetchBreedDescription = function(breedName, callback) {
+  const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
 request(url , (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  if(error=== null) {console.log("page not found"); return ;}
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  //if(error) return error;
+ //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   //console.log('body:', body); // Print the HTML for the example homepage
   const data = JSON.parse(body);
-  console.log(data);
-console.log(data[0].description);
-console.log(typeof data);
+  //console.log(data);
+let desc;
+  if(data[0] == undefined) {
+    error = true;
+    desc = null;
+    callback(error,desc);
+    return;
+  }
+  desc = data[0].description;
+
+callback(error,desc);
+
 })
+
+}
+module.exports = { fetchBreedDescription };
